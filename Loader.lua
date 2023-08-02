ffb47a2926566cf6e3f4ca21b368af6ec06bbe34
JamBourn123 <87535150+JamBourn123@users.noreply.github.com>
2023-08-02 20:22:42 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,62 +1,47 @@
-repeat task.wait() until game.IsLoaded
-repeat task.wait() until game.GameId ~= 0
+game.Loaded:Wait()
 
+local Parvus = getgenv().Parvus
 if Parvus and Parvus.Loaded then
     Parvus.Utilities.UI:Notification({
         Title = "Parvus Hub",
         Description = "Script already running!",
         Duration = 5
-    }) return
+    })
+    return
 end
 
---[[if Parvus and (Parvus.Game and not Parvus.Loaded) then
-    Parvus.Utilities.UI:Notification({
-        Title = "Parvus Hub",
-        Description = "Something went wrong!",
-        Duration = 5
-    }) return
-end]]
-
 local PlayerService = game:GetService("Players")
-repeat task.wait() until PlayerService.LocalPlayer
 local LocalPlayer = PlayerService.LocalPlayer
 
-local Branch,NotificationTime,IsLocal = ...
-local QueueOnTeleport = queue_on_teleport
-or (syn and syn.queue_on_teleport)
+local Branch, NotificationTime, IsLocal = ...
 
 local function GetFile(File)
-    return IsLocal and readfile("Parvus/" .. File)
-    or game:HttpGet(("%s%s"):format(Parvus.Source,File))
+    return IsLocal and readfile("Parvus/" .. File) or game:HttpGet(("%s%s"):format(Parvus.Source, File))
 end
 
 local function LoadScript(Script)
-    return loadstring(GetFile(Script .. ".lua"),Script)()
+    return loadstring(GetFile(Script .. ".lua"), Script)()
 end
 
 local function GetGameInfo()
-    for Id,Info in pairs(Parvus.Games) do
+    for Id, Info in pairs(Parvus.Games) do
         if tostring(game.GameId) == Id then
             return Info
         end
     end
-
     return Parvus.Games.Universal
 end
 
 getgenv().Parvus = {
-    Source = "https://raw.githubusercontent.com/AlexR32/Parvus/" .. Branch .. "/",
-
+    Source = "https://raw.githubusercontent.com/JamBourn123/Parvus/" .. Branch .. "/",
     Games = {
-        ["Universal" ] = {Name = "Universal",                 Script = "Universal" },
-        ["1168263273"] = {Name = "Bad Business",              Script = "Games/BB"  },
-        ["3360073263"] = {Name = "Bad Business PTR",          Script = "Games/BB"  },
-        ["1586272220"] = {Name = "Steel Titans",              Script = "Games/ST"  },
-        ["580765040" ] = {Name = "RAGDOLL UNIVERSE",          Script = "Games/RU"  },
-        ["187796008" ] = {Name = "Those Who Remain",          Script = "Games/TWR" },
-        --["358276974" ] = {Name = "Apocalypse Rising 2",       Script = "Games/AR2" },
-        --["3495983524"] = {Name = "Apocalypse Rising 2 Dev.",  Script = "Games/AR2" },
-        ["1054526971"] = {Name = "Blackhawk Rescue Mission 5",Script = "Games/BRM5"}
+        ["Universal"] = {Name = "Universal", Script = "Universal"},
+        ["1168263273"] = {Name = "Bad Business", Script = "Games/BB"},
+        ["3360073263"] = {Name = "Bad Business PTR", Script = "Games/BB"},
+        ["1586272220"] = {Name = "Steel Titans", Script = "Games/ST"},
+        ["580765040"] = {Name = "RAGDOLL UNIVERSE", Script = "Games/RU"},
+        ["187796008"] = {Name = "Those Who Remain", Script = "Games/TWR"},
+        ["1054526971"] = {Name = "Blackhawk Rescue Mission 5", Script = "Games/BRM5"}
     }
 }
 
@@ -67,14 +52,11 @@ Parvus.Utilities.Drawing = LoadScript("Utilities/Drawing")
 
 Parvus.Cursor = GetFile("Utilities/ArrowCursor.png")
 Parvus.Loadstring = GetFile("Utilities/Loadstring")
-Parvus.Loadstring = Parvus.Loadstring:format(
-    Parvus.Source,Branch,NotificationTime,
-    tostring(IsLocal)
-)
+Parvus.Loadstring = Parvus.Loadstring:format(Parvus.Source, Branch, NotificationTime, tostring(IsLocal))
 
 LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.InProgress then
-        QueueOnTeleport(Parvus.Loadstring)
+        syn.queue_on_teleport(Parvus.Loadstring)
     end
 end)
 
@@ -84,4 +66,5 @@ Parvus.Utilities.UI:Notification({
     Title = "Parvus Hub",
     Description = Parvus.Game.Name .. " loaded!",
     Duration = NotificationTime
-}) Parvus.Loaded = true
+})
+Parvus.Loaded = true
